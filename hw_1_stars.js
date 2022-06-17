1f0c38173ac87d07cfecd1df87bfaf47f8d0b55e
Denis Levdanskiy <d.levdanskiy@gmail.com>
2022-06-17 13:18:53 +0400

    hw 1 javascript

--- hw_1_stars.js
+++ hw_1_stars.js
@@ -59,33 +59,33 @@
 /*3**:
 Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number*/
 
-let age_1 = 10;
+
 let age_2 = 18
 let age_3 = 60
 
-function checkage(age_1) {
+const div = document.getElementById('d')
 
-       
-    if (age_1 == String) {
-        // parseInt(age_1.match(/\d+/)) 
-        Number(age_1)      
-    }  
-       
-    if (age_1 < age_2){
-        console.log("You don't have access cause your age is " + age_1 + " It’s less then")
-    } else if (age_1 >= age_2 & age_1 < age_3){
-        console.log("Welcome")
-    } else if (age_1 > age_3){
-        console.log("Keep calm and look Culture channel")
-    } else {
-        console.log("Technical work")
-    }
+function checkage(...age) {
+    age.forEach(el => {
+        if (el && !isNaN(el)){
+            if (el < age_2){
+                div.innerHTML += "You don't have access cause your age is " + el + " It’s less then" + '<br />';
+            } else if (el >= age_2 && el < age_3){
+                div.innerHTML += "Welcome" + '<br />' 
+            } else if (el > age_3){
+                div.innerHTML += "Keep calm and look Culture channel" + '<br />'
+            } else {
+                div.innerHTML += "Technical work" + '<br />'
+            }
+        } else  div.innerHTML += "Not value" + '<br />'
+    })
 }
 
 
-checkage("2")
-checkage('18')
-checkage(61)
+// let a = prompt("Enter your age")
+
+checkage(17, 18, 60, '', '0', '22', 'fkj')
+
 
 
 
